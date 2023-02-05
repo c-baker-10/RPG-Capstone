43ad9d9bb2b4d50e4d40d58462dfd4a73b6a234f
c-baker-10 <113151545+c-baker-10@users.noreply.github.com>
2023-02-05 15:40:20 -0500

    Added a user object and updated data storing format

---
+++ BlazorApp1/Objects/User.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Objects
+{
+    public class User
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string General { get; set; }
+        public string Badges { get; set; }
+        public string Items { get; set; }
+        public string Stuff { get; set; }
+
+        public string[] Grades { get; set; }
+        public string Level { get; set; }
+
+    }
+}
