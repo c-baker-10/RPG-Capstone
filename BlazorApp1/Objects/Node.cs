b1af8625a2ebbcc7efc1c254c19b64f7b6617356
c-baker-10 <113151545+c-baker-10@users.noreply.github.com>
2023-02-07 20:44:34 -0500

    Updated Skill Tree Maker UI

--- BlazorApp1/Objects/Node.cs
+++ BlazorApp1/Objects/Node.cs
@@ -17,8 +17,11 @@
         public string yPosistion { get; set; }
         public string width { get; set; }
         public string height { get; set; }
+        public string EducationType { get; set; }
 
         public bool startingNode = false;
+        public bool userAt = false;
+
 
     }
 }
