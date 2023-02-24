2df95fb771871cda48e3aa87cb4af096cb208419
c-baker-10 <113151545+c-baker-10@users.noreply.github.com>
2023-02-23 19:38:09 -0500

    Gets user input for questions

--- BlazorApp1/Objects/AssignmentQuestion.cs
+++ BlazorApp1/Objects/AssignmentQuestion.cs
@@ -11,16 +11,26 @@
         public string type { get; set; }
         public string[] options { get; set; }
         public string[] answers { get; set; }
-        public string inputedAnswers { get; set; }
+        public string[] inputedAnswers { get; set; }
         public bool correct { get; set; }
 
-        public void correctInput(string[] answer)
+        public void correctInput()
         {
-            if( this.answers == answer)
+            if(answers.Length != inputedAnswers.Length)
             {
-                correct = true;
+                correct = false;
+                return;
             }
-            correct = false;
+
+            foreach(string ans in answers)
+            {
+                if(!inputedAnswers.Contains(ans))
+                {
+                    correct = false;
+                    return;
+                }
+            }
+            correct = true;
         }
     }
 }
--- BlazorApp1/wwwroot/JSFolder/UserInput.js
+++ BlazorApp1/wwwroot/JSFolder/UserInput.js
@@ -4,7 +4,24 @@
 
     for (i = 0; i < ele.length; i++) {
         if (ele[i].checked)
-            return ele[i].value;
+            return ele[i].value +";";
     }
     return "none";
+}
+
+function CheckBoxs(name) {
+    var checkboxes = document.getElementsByName(name);
+    var result = "";
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            result += checkboxes[i].value + ";";
+        }
+    }
+    return result
+}
+
+function TextFeild(name) {
+    var ans = document.getElementsByName(name)[0].value;
+    return ans;
 }
