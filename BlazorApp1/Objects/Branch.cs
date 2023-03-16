a57385815f54787321e034b5afd2ce94743d6fac
c-baker-10 <113151545+c-baker-10@users.noreply.github.com>
2023-03-16 13:16:23 -0400

    Making and displaying skill trees Version 2

---
+++ BlazorApp1/Objects/Branch.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorApp1.Objects
+{
+    public class Branch
+    {
+        public string name;
+
+        public List<Branch> NodeConnections = new List<Branch>();
+
+        public Branch(string name)
+        {
+            this.name = name;
+        }
+
+        public string ID { get; set; }
+        public string description { get; set; }
+        public string links { get; set; }
+        public string xPosistion { get; set; }
+        public string yPosistion { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+        public string EducationType { get; set; }
+        public string entryScore { get; set; }
+
+        public bool startingNode = false;
+        public bool userAt = false;
+
+
+    }
+}
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddSyncfusionBlazor();
+
 var app = builder.Build();
+
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt/QHRqVVhlXVpAaV5AQmFJfFBmRmldeVR0dEUmHVdTRHRcQl5iSn9WckJiWn1deHY=;Mgo+DSMBPh8sVXJ0S0J+XE9Bd1RGQmJLYVF2R2BJfFR0cl9CYEwxOX1dQl9gSX1SdURmXXhfc3ZQQmI=;ORg4AjUWIQA/Gnt2VVhkQlFac15JWXxIfEx0RWFab116cFNMZFxBJAtUQF1hSn5Qd0ZiXHtZcXdXQGle;MTMxNTIxMEAzMjMwMmUzNDJlMzBXL2k4czVWRnpxdzJLUTVtbUVIRXJReUVTV1hpS0lFZGJFcVh0OGRhMDhrPQ==;MTMxNTIxMUAzMjMwMmUzNDJlMzBsWExGSkxDUU1SSndOSVF2R0NFcVd4V0NvZjgxM3lzOUlaaXZCTWpaTVpNPQ==;NRAiBiAaIQQuGjN/V0Z+WE9EaFpDVmdWf1FpR2NbfE5zflFAalhRVBYiSV9jS31TdURgWHpad3VWRWNVUw==;MTMxNTIxM0AzMjMwMmUzNDJlMzBCVDR5RDl4Q0M5RXdndWttSmJiYndkdHhUWDdsNEw4OWYyTWVNbTA3V0RvPQ==;MTMxNTIxNEAzMjMwMmUzNDJlMzBhQVBiT3JheUk3UGJ1UzdBNE5INC95bHZ3N2thNVdxazZTMnlzU2ZxL0VvPQ==;Mgo+DSMBMAY9C3t2VVhkQlFac15JWXxIfEx0RWFab116cFNMZFxBJAtUQF1hSn5Qd0ZiXHtZcXdRRmFe;MTMxNTIxNkAzMjMwMmUzNDJlMzBqd2l4YVYwVVJjbHZRc2pkZHNFREpHeTRrSSsvUEMrNzRMN0EvODR1bDBVPQ==;MTMxNTIxN0AzMjMwMmUzNDJlMzBqUkgrVG1nSlhDaTJMSXhaQ2xFMHUxaTA3c1VrM0RGeWZmR1lGSUR4cUhrPQ==;MTMxNTIxOEAzMjMwMmUzNDJlMzBCVDR5RDl4Q0M5RXdndWttSmJiYndkdHhUWDdsNEw4OWYyTWVNbTA3V0RvPQ==");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
