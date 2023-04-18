5e176dff159cff44cf1d98af72a6b91986c782f8
caden-coding <116124430+caden-coding@users.noreply.github.com>
2023-04-18 08:30:02 -0400

    file upload button completed (note: doesnt actually get processed, this is just front end)

--- BlazorApp1/wwwroot/JSFolder/upload.js
+++ BlazorApp1/wwwroot/JSFolder/upload.js
@@ -1,55 +1,29 @@
-﻿
-const dropArea = document.querySelector(".drag-area"),
-    dragText = dropArea.querySelector("header"),
-    button = dropArea.querySelector("button"),
-    input = dropArea.querySelector("input");
-let file; 
+﻿Array.prototype.forEach.call(
+    document.querySelectorAll(".file-upload__button"),
+    function (button) {
+        const hiddenInput = button.parentElement.querySelector(
+            ".file-upload__input"
+        );
+        const label = button.parentElement.querySelector(".file-upload__label");
+        const defaultLabelText = "No file(s) selected";
 
-button.onclick = () => {
-    input.click(); 
-}
+        // Set default text for label
+        label.textContent = defaultLabelText;
+        label.title = defaultLabelText;
 
-input.addEventListener("change", function () {
+        button.addEventListener("click", function () {
+            hiddenInput.click();
+        });
 
-    file = this.files[0];
-    dropArea.classList.add("active");
-    showFile(); 
-});
+        hiddenInput.addEventListener("change", function () {
+            const filenameList = Array.prototype.map.call(hiddenInput.files, function (
+                file
+            ) {
+                return file.name;
+            });
 
-dropArea.addEventListener("dragover", (event) => {
-    event.preventDefault(); 
-    dropArea.classList.add("active");
-    dragText.textContent = "Release to Upload File";
-});
-
-
-dropArea.addEventListener("dragleave", () => {
-    dropArea.classList.remove("active");
-    dragText.textContent = "Drag & Drop to Upload File";
-});
-
-
-dropArea.addEventListener("drop", (event) => {
-    event.preventDefault(); 
-
-    file = event.dataTransfer.files[0];
-    showFile(); 
-});
-
-function showFile() {
-    let fileType = file.type; 
-    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-    if (validExtensions.includes(fileType)) { 
-        let fileReader = new FileReader(); 
-        fileReader.onload = () => {
-            let fileURL = fileReader.result; 
-            let imgTag = `<img src="${fileURL}" alt="image">`; 
-            dropArea.innerHTML = imgTag; 
-        }
-        fileReader.readAsDataURL(file);
-    } else {
-        alert("This is not an Image File!");
-        dropArea.classList.remove("active");
-        dragText.textContent = "Drag & Drop to Upload File";
+            label.textContent = filenameList.join(", ") || defaultLabelText;
+            label.title = label.textContent;
+        });
     }
-}
+);
